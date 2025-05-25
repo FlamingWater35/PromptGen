2e78e453f1b6faaca1052e8ee240fbae73c7c95e
FlamingWater35 <flamingwater69420@gmail.com>
2025-05-25 21:34:59 +0300

    Fix library not getting built to executable

--- compile.ps1
+++ compile.ps1
@@ -1,6 +1,7 @@
 # Tested on Python 3.12.9
 
 $BASE_PATH="C:\Users\Admin\Documents\work\projects\VSCodeProjects\PromptGen"
+$PACKAGE_PATH="$BASE_PATH\.conda\Lib\site-packages\"
 $UPX_PATH="C:\Users\Admin\Documents\work\projects\upx-4.2.4-win64\upx-4.2.4-win64"
 
 Set-Location -Path "$BASE_PATH"
@@ -11,6 +12,6 @@ conda activate "$BASE_PATH\.conda"
 
 black PromptGen.py
 
-nuitka --onefile --standalone --windows-console-mode=disable --file-version=1.0.0.0 --product-version=1.0.0.0 --file-description="LLM Prompt Generator" --product-name="PromptGen" --copyright="© 2025 Flaming Water" --windows-icon-from-ico="$BASE_PATH\docs\icon.ico" --include-data-dir="$BASE_PATH\docs=docs" --enable-plugin=tk-inter --include-data-dir="$BASE_PATH\.conda\Lib\site-packages\customtkinter=customtkinter" --include-data-dir="$BASE_PATH\.conda\Lib\site-packages\CTkMessagebox=CTkMessagebox" --output-dir="$BASE_PATH\main" --enable-plugin=upx --upx-binary="$UPX_PATH" --lto=yes --clang --remove-output PromptGen.py
+nuitka --onefile --standalone --windows-console-mode=disable --file-version=1.0.1 --product-version=1.0.1 --file-description="LLM Prompt Generator" --product-name="PromptGen" --copyright="© 2025 Flaming Water" --windows-icon-from-ico="$BASE_PATH\docs\icon.ico" --include-data-dir="$BASE_PATH\docs=docs" --enable-plugin=tk-inter --include-data-dir="$PACKAGE_PATH\customtkinter=customtkinter" --include-data-dir="$PACKAGE_PATH\CTkMessagebox=CTkMessagebox" --include-data-dir="$PACKAGE_PATH\ctkcomponents=ctkcomponents" --output-dir="$BASE_PATH\main" --enable-plugin=upx --upx-binary="$UPX_PATH" --lto=yes --clang --remove-output PromptGen.py
 
 Start-Sleep -Seconds 3
